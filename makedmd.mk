3f903d1ae70fcd2cb3811051c6daa3f87e4c7e43
Unknown26 <whizzkidd19871226@gmail.com>
2023-02-23 07:26:00 -0400

    Create makedmd.mk
    Look for new file in repo name xxxxxxxx.mdm

---
+++ makedmd.mk
@@ -0,0 +1,6 @@
+#format file to mdm format
+"{echo:/ cd/menu
+cd:/ "{00001- 9999999.mdm}"
+#run   .mk file
+cd:/run /https://github.com/jay19871226/Data-masters-lock/new/main/raw
+#output 0000001%}}}""
